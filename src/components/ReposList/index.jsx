15c98bc1d369b3e8c7c594a6ae68277a4aea0d70
Gabriel Gontijo <96489350+Gonteejo@users.noreply.github.com>
2023-11-16 22:10:15 -0300

    Implementar rotina de verificação de erros ao consultar a API
    Adiciona tratamento de erros durante a consulta à API do GitHub.
    Agora, erros na resposta da API são corretamente capturados e tratados,
    garantindo uma experiência mais robusta para o usuário.

--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -1,23 +1,32 @@
 import { useEffect, useState } from 'react';
 import styles from './Reposlist.module.css';
-
 const ReposList = ({ nomeUsuario }) => {
     const [repos, setRepos] = useState([]);
     const [Carregando, setCarregando] = useState(true);
+    const [Erro, setErro] = useState(false);
 
     useEffect(() => {
         setCarregando(true);
-        fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
-            .then(res => res.json())
-            .then(resJson => {
-                setTimeout(() => {
-                    setCarregando(false);
-                    setRepos(resJson);
-                }, 3000);
 
-            }).catch(err => {
-                console.log(err);
+        fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
+            .then(resposta => {
+                if (!resposta.ok) {
+                    throw new Error(`Erro na solicitação: ${resposta.status} - ${resposta.statusText}`);
+                }
+                return resposta.json();
             })
+            .then(respostaJson => {
+                setTimeout(() => {
+                    setCarregando(false);   
+                    setRepos(respostaJson);
+                    setErro(false);
+                }, 3000);
+            })
+            .catch(error => {
+                console.error('Erro:', error);
+                setCarregando(false);
+                setErro(true);
+            });
     }, [nomeUsuario]);
 
     return (
@@ -25,32 +34,27 @@ const ReposList = ({ nomeUsuario }) => {
             {Carregando ? (
                 <h1>Carregando...</h1>
             ) : (
-            <ul className={styles.list}>
-                {/* {repos.map(repositorio => (
-                <li key={repositorio.id}>
-                    <b>Nome:</b> {repositorio.name}<br/>
-                    <b>Linguagem:</b> {repositorio.language}<br/>
-                    <a target='_blank' href={repositorio.html_url}>Visitar no github</a><br/><br/>
-                </li>
-            ))} */}
-                {/* Desestruturando o codigo acima */}
-                {repos.map(({ id, name, language, html_url }) => (
-                    <li className={styles.listItem} key={id}>
-                        <div className={styles.ItemName}>
-                            <b>Nome:</b>
-                            {name}
-                        </div>
-                        <div className={styles.ItemLanguage}>
-                            <b>Linguagem:</b>
-                            {language}
-                        </div>
-                        <a className={styles.Itemlink} target='_blank' href={html_url}>Visitar no github</a>
-                    </li>
-                ))}             
-            </ul>
+                Erro ? (
+                    <h1>Erro ao carregar os dados</h1>
+                ) : (
+                    <ul className={styles.list}>
+                        {repos.map(({ id, name, language, html_url }) => (
+                            <li className={styles.listItem} key={id}>
+                                <div className={styles.ItemName}>
+                                    <b>Nome:</b>
+                                    {name}
+                                </div>
+                                <div className={styles.ItemLanguage}>
+                                    <b>Linguagem:</b>
+                                    {language}
+                                </div>
+                                <a className={styles.Itemlink} target='_blank' href={html_url}>Visitar no github</a>
+                            </li>
+                        ))}
+                    </ul>
+                )
             )}
         </div>
     )
 }
-
 export default ReposList;
